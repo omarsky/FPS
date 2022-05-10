2050dcb827ec6a915f395db5e678c988787db0b1
omar <o.ordenidze@gmail.com>
2022-05-10 22:49:16 +0300

    - Added gravity for the player - Refactored rotation, to exclude rotation of player object around X axis

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,8 @@ public class PlayerController : MonoBehaviour
     float m_rotationSpeed = 1f;
 
     CharacterController m_controller;
+    [SerializeField]
+    Transform m_armCamera;
 
     [SerializeField]
     GameObject m_bulletSpawn;
@@ -24,6 +26,18 @@ public class PlayerController : MonoBehaviour
     Vector3 m_currentMove;
     Vector3 m_currentRotation;
 
+    [Header("Ground check")]
+    [SerializeField]
+    Transform m_checkGround;
+    [SerializeField]
+    float m_checkGroundRadius = 2f;
+    [SerializeField]
+    float m_gravity = 9.81f;
+    Vector3 m_currentSpeed;
+    bool m_isGrounded;
+    [SerializeField]
+    public LayerMask m_groundMask;
+
     void Awake()
     {
         m_controller = GetComponent<CharacterController>();
@@ -34,13 +48,35 @@ public class PlayerController : MonoBehaviour
     {
         UpdateRotation();
         UpdateMovement();
+        UpdateGravity();
+    }
+
+    private void UpdateGravity()
+    {
+        if (!m_isGrounded)
+        {
+            m_currentSpeed.y -= (m_gravity / 2) * (Time.deltaTime * Time.deltaTime);
+            m_controller.Move(m_currentSpeed);
+        }
+        if (Physics.CheckSphere(m_checkGround.position, m_checkGroundRadius, m_groundMask))
+        {
+            m_isGrounded = true;
+            m_currentSpeed.y = 0;
+        }
+        else
+        {
+            m_isGrounded = false;
+        }
     }
 
     private void UpdateRotation()
     {
-        Vector3 currentRot = transform.rotation.eulerAngles;
-        Vector3 newRot = new Vector3(currentRot.x - m_currentRotation.y * m_rotationSpeed * Time.deltaTime, currentRot.y + m_currentRotation.x * m_rotationSpeed * Time.deltaTime, 0);
-        transform.rotation = Quaternion.Euler(newRot);
+        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y + m_currentRotation.x * m_rotationSpeed * Time.deltaTime, 0);
+        
+        Vector3 newRot = new Vector3(Camera.main.transform.rotation.eulerAngles.x - m_currentRotation.y * m_rotationSpeed * Time.deltaTime, transform.rotation.eulerAngles.y, 0);
+        Quaternion camerasRotation = Quaternion.Euler(newRot);
+        Camera.main.transform.rotation = camerasRotation;
+        m_armCamera.rotation = camerasRotation;
     }
 
     private void UpdateMovement()
@@ -50,7 +86,6 @@ public class PlayerController : MonoBehaviour
         m_controller.Move(moveForward);
         m_controller.Move(moveSideways);
     }
-
 
     public void OnMove(InputAction.CallbackContext value)
     {
@@ -83,5 +118,10 @@ public class PlayerController : MonoBehaviour
         {
             GetComponent<HealthComponent>().ReceiveDamage(20f);
         }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawSphere(m_checkGround.position, m_checkGroundRadius);
     }
 }
